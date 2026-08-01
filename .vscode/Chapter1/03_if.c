660a50141de5bfbd18ac639fdec1924e79ed52b9
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-01 13:05:10 +0530

    New

--- .vscode/Chapter1/03_if.c
+++ .vscode/Chapter1/03_if.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main() {
-    int x = 10;
-    if(x > 4) {
-        printf("x is greater than 5");
+    int y = 10;
+    if(y > 4) {
+        printf("y is greater than 5");
     }
     return 0;
 }
