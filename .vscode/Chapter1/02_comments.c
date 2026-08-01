bd0b7b5ac9ce32141f8641466e94eed24cbb56ec
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-02 00:18:29 +0530

    changes

---
+++ .vscode/Chapter1/02_comments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main () {
+     
+// double backslash for single line comment
+/* this is used for multiline comments 
+    with the help of forward slash and asterisk 
+    we can write multiplr line comment
+    */
+    printf("Hello world");
+    return 0;
+
+}
--- .vscode/Chapter1/03_if.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main() {
-    int y = 10;
-    if(y > 4) {
-        printf("y is greater than 5");
-    }
-    return 0;
-}
