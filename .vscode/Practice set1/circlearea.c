70eef69d4f941ab77cf71727cc057a66d1774847
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-03 19:02:19 +0530

    circlearea

---
+++ .vscode/Practice set1/circlearea.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float r; // radius
+    float π = 22.0/7.0; // pi
+    printf("the value of radius\n");
+    scanf("%f", &r);
+    printf("area of circle is %f", π * r * r);
+    return 0;
+}
