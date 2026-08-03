07e98692f5ce43a3fd010eedbdb41ce3cfc079c0
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-03 18:45:14 +0530

    modified area

--- .vscode/Practice set1/area.c
+++ .vscode/Practice set1/area.c
@@ -3,8 +3,10 @@ int main () {
  int a; // lendth 
  int b; //breadth
 
- printf("enter the value of a&b");
- scanf("%d%d", &a, &b);
+ printf("enter the value of a\n");
+ scanf("%d", &a);
+ printf("enter the value of b\n");
+ scanf("%d" ,&b);
 
  printf("area of rectangle is %d", a*b);
  return 0;
