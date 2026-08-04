c732553268a398042eb14406f47e329cdb068279
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-05 00:45:09 +0530

    arithematic instruction

---
+++ .vscode/chapter2/03_arithmetic2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    // + addition
+    // - substraction
+    // * multiplication
+    // / division
+    // % modulus
+
+    int a = 10;
+    int b = 5;
+
+    printf("the sum of a and b is %d\n ", a + b);
+    printf("the difference of a and b is %d\n ", a - b);
+    printf("the product of a and b is %d\n ", a * b);
+    printf("the division of a and b is %d \n", a / b);
+    printf("the remainder of a and b is %d \n", a % b);
+
+    return 0;
+}
