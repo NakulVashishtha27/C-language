f27cd5650bab2d04731f4b20b08f188b9e9e4c3f
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-05 00:29:18 +0530

    variableo1

---
+++ .vscode/chapter2/01_variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a; // variable initialization
+    a = 10; // variable declaration
+  float  b = a; // variable declaration and initialization
+printf("the value of a is %d\nand the value of b is %f ",a , b );
+return 0;
+}
---
+++ .vscode/chapter2/02_arithmetic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10; // variable declaration and initialization
+    int b = 20; // variable declaration and initialization
+
+    int sum = a + b; // addition
+   // scanf("%d%d",&a,&b);
+    printf("the sum of a and b is %d",sum );
+    return 0;
+}
