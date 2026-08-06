0d8fd42d0a950b82064a5c1109afb2ca394fbf94
NakulVashishtha27 <vashishthanakul27@gmail.com>
2026-08-06 23:40:51 +0530

    rule

---
+++ .vscode/chapter2/Rule_for_Arithmatic.c
@@ -0,0 +1,11 @@
+// x * y / z → (x * y) / z
+
+// x / y * z → (x / y) * z
+
+// * and / follow left to righ
+
+/*Priorit                 operators
+
+1st                        * / %
+2nd.                        + -
+3rd                          =   */
