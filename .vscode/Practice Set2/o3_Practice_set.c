aae8602a42e9302741c7ffbecad661ef6dc38808
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-12 10:48:41 +0530

    Problem

--- .vscode/Practice Set2/o3_Practice_set.c
+++ .vscode/Practice Set2/o3_Practice_set.c
@@ -2,7 +2,16 @@
 
 int main()
 {
-   
-   
+    // Calculate 3*x/y-z+k , where x = 2 , y = 3 , z = 3 , k = 1 .
+    int x = 2, y = 3, z = 3, k = 1;
+    float a = 3 * x / y - z + k;
+    printf("the value of a is %f \n", a);
+
+    // Solution
+    // 3*x/y-z=k = 3*2/y-z+k = 6/y-z+k
+    // 2-3+k
+    // -1+k
+    // 0
+
     return 0;
 }
