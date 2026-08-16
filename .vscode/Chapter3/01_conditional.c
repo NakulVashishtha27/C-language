218a5af6db251f0f92eda612a69955c65bec2be1
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-16 18:39:12 +0530

    Conditional

---
+++ .vscode/Chapter3/01_conditional.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+
+int age =10;
+
+if(age>9){
+
+    printf("we are inside if\n");
+    printf("your age is greater than 9\n");
+
+}
+if(age>8){
+    printf("we are inside if\n");
+    printf("your age is greater than 8\n");
+}
+    return 0;
+}
