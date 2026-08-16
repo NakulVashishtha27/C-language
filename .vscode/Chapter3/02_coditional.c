0f84a4703d27b3678cb904175cdae0a80b2b252f
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-16 18:54:25 +0530

    Eligibility

---
+++ .vscode/Chapter3/02_coditional.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+
+int age =18;
+if(age>=18){
+    printf("you are eligible to vote\n");
+    printf("you can drive\n");
+    printf("you can marry\n");
+}
+else{
+    printf("you are not eligible to vote\n");
+    printf("you can not drive\n");
+    printf("you can not marry\n");
+    
+
+
+}
+    return 0;
+}
