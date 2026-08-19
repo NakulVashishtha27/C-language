74849a1e467c3613d6ce0939411e8d40b669d689
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-19 11:40:50 +0530

    ternary

---
+++ .vscode/Chapter3/04_ternary.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+
+    int a,b ;
+    printf("The value od a and b is :");
+    scanf("%d%d",&a,&b);
+    a>b?printf("a is greater than b"):printf("b is greater than a");
+
+return 0;
+
+}
