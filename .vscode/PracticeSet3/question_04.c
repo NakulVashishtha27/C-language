4c0deb2f5283a8cb8f90ff00eb0ae40e0738107b
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-08-30 00:16:42 +0530

    Leap year

---
+++ .vscode/PracticeSet3/question_04.c
@@ -0,0 +1,32 @@
+/* Write a program to find whether a year entered by the user is a leap year or not. Take
+year as an input from the user? */
+
+#include <stdio.h>
+int main()
+{
+int year;
+//scanf("%d",&year);
+printf("Enter year :");
+scanf("%d",&year);
+
+if(year %4==0 && year %100!=0 || year%400 ==0){
+
+    printf("given year is leap year\n");
+
+}
+else{
+    printf("Given year is not leap year");
+
+
+}
+
+
+
+
+
+
+
+
+
+    return 0;
+}
