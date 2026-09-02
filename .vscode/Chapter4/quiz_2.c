a280d7d027343dd9a8580857f171f89607c60add
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-09-02 11:56:13 +0530

    Changes

--- .vscode/Chapter4/quiz_2.c
+++ .vscode/Chapter4/quiz_2.c
@@ -11,9 +11,8 @@ int main(){
 int  i=0;
 while(i<=20){
     if(i>=10){
-        printf("the value of i is %d\n",i);
-    }
-    i++;   
+    printf("the value of i is %d\n",i);
+     } i++;   
  } return  0;
 
 
