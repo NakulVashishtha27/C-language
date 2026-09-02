3b32c06cccdbd76a3ea3c2e4af0d5222e0582a17
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-09-02 23:57:07 +0530

    quiz

---
+++ .vscode/Chapter4/02_do_while.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int  i,n;
+    scanf("%d",&n);
+    i=1;
+    do{
+printf("the value of i is %d\n",i);
+        i++;
+
+    }while(i<=n);
+
+
+
+    return 0;
+}
