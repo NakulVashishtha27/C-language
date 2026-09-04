2f9fb357dd0fe876ee65a30013271dc3e66ac679
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-09-05 00:28:06 +0530

    quick quiz

---
+++ .vscode/Chapter4/quick_quiz.c
@@ -0,0 +1,15 @@
+// Quick Quiz: Write a program to print first 'n' natural number using do-while loop.
+#include <stdio.h>
+int main(){
+ 
+    int n=4;
+    for(int i=1; i<=n; i++){
+        printf(" %d\n", i);
+    }
+
+return 0;
+
+
+}
+
+
