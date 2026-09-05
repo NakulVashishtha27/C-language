3e103f09719ee1f1f06d04eed21c9078382f493b
Nakul Vashishtha <vashishthanakul27@gmail.com>
2026-09-05 15:25:29 +0530

    Problem.c

---
+++ .vscode/Practice Set4/problem_02.c
@@ -0,0 +1,14 @@
+// 2. Write a program to print multiplication table of 10 in reversed order.
+
+#include<stdio.h>
+int main(){
+    int n =10;
+    printf("Multiplication table of 10 in reversed order:\n");
+    for(int i=10; i; i--){
+        printf("%d x %d = %d\n", n, i, n*i);
+    }
+
+
+
+    return 0;
+}
